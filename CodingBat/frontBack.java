c3a817fdfda25211c80638100f676b7b0518004d
Gozzerks <VivaldoDjol@users.noreply.github.com>
2025-05-29 13:01:22 +0100

    Solve frontBack.java

---
+++ CodingBat/frontBack.java
@@ -0,0 +1,8 @@
+public String frontBack(String str) {
+    int len = str.length();
+
+    if (len == 0) return "";
+    if (len == 1) return str;
+
+    return str.charAt(len - 1) + str.substring(1, len - 1) + str.charAt(0);
+}
