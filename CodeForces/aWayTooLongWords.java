84f31a9e268aee78fbb5cfa06646550ea2a61a8e
Gozzerks <VivaldoDjol@users.noreply.github.com>
2025-06-02 12:42:32 +0100

    Solve aWayTooLongWords.java

---
+++ CodeForces/aWayTooLongWords.java
@@ -0,0 +1,74 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) {
+        MyScanner sc = new MyScanner();
+        out = new PrintWriter(new BufferedOutputStream(System.out));
+
+        // Start writing your solution here. -------------------------------------
+
+        int n = sc.nextInt();
+
+        for (int i = 0; i < n; i++) {
+            String line = sc.nextLine();
+            if (line.length() <= 10) {
+                out.println(line);
+            } else {
+                String wordAbbrev = "" + line.charAt(0) + (line.length() - 2) + line.charAt(line.length() - 1);
+                out.println(wordAbbrev);
+            }
+        }
+        // Stop writing your solution here. -------------------------------------
+        out.close();
+    }
+
+
+    //-----------PrintWriter for faster output---------------------------------
+    public static PrintWriter out;
+
+    //-----------MyScanner class for faster input----------
+    public static class MyScanner {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public MyScanner() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+
+    }
+    //--------------------------------------------------------
+}
