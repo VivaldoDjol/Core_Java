0158ce47c85a4574310d122b58f076c1e3b8bf10
Gozzerks <VivaldoDjol@users.noreply.github.com>
2025-06-06 13:18:00 +0100

    Solve aDominoPiling.java

---
+++ CodeForces/aDominoPiling.java
@@ -0,0 +1,80 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) {
+        MyScanner sc = new MyScanner();
+        out = new PrintWriter(new BufferedOutputStream(System.out));
+
+        // Start writing your solution here. -------------------------------------
+        int m = sc.nextInt();
+        int n = sc.nextInt();
+        out.println(m * n / 2);
+        // Stop writing your solution here. -------------------------------------
+        out.close();
+    }
+
+    //-----------PrintWriter for faster output---------------------------------
+    public static PrintWriter out;
+
+    //-----------MyScanner class for faster input----------
+    public static class MyScanner {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public MyScanner() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+
+    }
+    //--------------------------------------------------------
+}
+
+
+/// /// This is a Bit Faster
+//import java.io.BufferedReader;
+//import java.io.InputStreamReader;
+//import java.util.StringTokenizer;
+//
+//public class Main {
+//    public static void main(String[] args) throws Exception {
+//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//        StringTokenizer st = new StringTokenizer(br.readLine());
+//        int m = Integer.parseInt(st.nextToken());
+//        int n = Integer.parseInt(st.nextToken());
+//        System.out.println(m * n / 2);
+//    }
+//}
