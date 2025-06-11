13f73264488017ab47c24956c2143728b45cfb79
Gozzerks <VivaldoDjol@users.noreply.github.com>
2025-06-11 13:15:00 +0100

    Solve aStonesOnTheTable.java

---
+++ CodeForces/aStonesOnTheTable.java
@@ -0,0 +1,72 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) {
+        MyScanner sc = new MyScanner();
+        out = new PrintWriter(new BufferedOutputStream(System.out));
+
+        // Start writing your solution here. -------------------------------------
+        int n = sc.nextInt();
+        String stones = sc.nextLine();
+        int removedCounter = 0;
+        char [] stonesToArray = stones.toCharArray();
+        for (int i = 0; i < n; i++) {
+            if (i < n-1 && stonesToArray[i] == stonesToArray[i+1] ) {
+                stonesToArray[i] = ' ';
+                removedCounter++;
+            }
+        }
+        out.println(removedCounter);
+        // Stop writing your solution here. -------------------------------------
+        out.close();
+    }
+
+    //-----------PrintWriter for faster output---------------------------------
+    public static PrintWriter out;
+
+    //-----------MyScanner class for faster input----------
+    public static class MyScanner {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public MyScanner() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+
+    }
+    //--------------------------------------------------------
+}
