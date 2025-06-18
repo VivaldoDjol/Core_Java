b5c07948f467d24c9f5eeb06f1a06bfcfd339d48
Gozzerks <VivaldoDjol@users.noreply.github.com>
2025-06-18 12:30:14 +0100

    Solve aTranslation.java

---
+++ CodeForces/aTranslation.java
@@ -0,0 +1,71 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) {
+        MyScanner sc = new MyScanner();
+        out = new PrintWriter(new BufferedOutputStream(System.out));
+
+        // Start writing your solution here. -------------------------------------
+        String word = sc.nextLine();
+        String wordReversed = sc.nextLine();
+        char[] wordArray = word.toCharArray();
+        StringBuilder output = new StringBuilder();
+        for (int i = word.length()-1; i > -1; i--) {
+            output.append(wordArray[i]);
+        }
+        if(wordReversed.contentEquals(output)) out.println("YES");
+        else out.println("NO");
+        ///output.toString().equals(wordReversed)
+        // Stop writing your solution here. -------------------------------------
+        out.close();
+    }
+
+    //-----------PrintWriter for faster output---------------------------------
+    public static PrintWriter out;
+
+    //-----------MyScanner class for faster input----------
+    public static class MyScanner {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public MyScanner() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+
+    }
+    //--------------------------------------------------------
+}
