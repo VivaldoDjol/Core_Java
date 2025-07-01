e8596d2172c7df1be6939911c441e2f051f20288
Gozzerks <VivaldoDjol@users.noreply.github.com>
2025-07-01 13:05:03 +0100

    Solve aHulk.java

---
+++ CodeForces/aHulk.java
@@ -0,0 +1,83 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        MyScanner sc = new MyScanner();
+        out = new PrintWriter(new BufferedOutputStream(System.out));
+
+        // Start writing your solution here. -------------------------------------
+        int n = sc.nextInt();
+
+        for (int i = 1; i <= n; i++) {
+            if (i % 2 != 0) {
+                System.out.print("I hate");
+            } else {
+                System.out.print("I love");
+            }
+
+            if (i == n) {
+                System.out.print(" it");
+            } else {
+                System.out.print(" that ");
+            }
+        }
+        // Stop writing your solution here. -------------------------------------
+        out.close();
+    }
+
+    public static void compareIntArrays(int[] arr1, int[] arr2, int[] arr3) {
+        for (int i = 0; i < arr1.length; i++) {
+            arr3[i] = (arr1[i] != arr2[i]) ? 1 : 0;
+        }
+        for (int num : arr3) {
+            out.print(num);
+        }
+    }
+    public static int[] readDigitArray(MyScanner sc) {
+        String line = sc.nextLine().trim();
+        int[] array = new int[line.length()];
+        for (int i = 0; i < line.length(); i++) {
+            array[i] = Character.getNumericValue(line.charAt(i));
+        }
+        return array;
+    }
+
+    // PrintWriter for faster output
+    public static PrintWriter out;
+
+    // Fast Scanner
+    public static class MyScanner {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public MyScanner() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+}
