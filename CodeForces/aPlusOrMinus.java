f68d3a897750aefc4abb582db815e7382df7d270
Gozzerks <VivaldoDjol@users.noreply.github.com>
2025-07-30 12:27:40 +0100

    Solve aPlusOrMinus.java

---
+++ CodeForces/aPlusOrMinus.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int tests = sc.nextInt();
+        for (int i = 0; i < tests; i++) {
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            int c = sc.nextInt();
+            if (a + b == c) {
+                System.out.println("+");
+            } else if (a - b == c) {
+                System.out.println("-");
+            }
+        }
+    }
+}
