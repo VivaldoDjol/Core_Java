ca66aba53f9c2dc92f096dd6a67d39c58dc27cf5
Gozzerks <VivaldoDjol@users.noreply.github.com>
2025-08-11 17:16:03 +0100

    Solve aDieRoll.java

---
+++ CodeForces/aDieRoll.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Main {
+    public static int gcd(int a, int b) {
+        while (b != 0) {
+            int temp = b;
+            b = a % b;
+            a = temp;
+        }
+        return a;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        int winningOutcomes = 7 - Math.max(a, b);
+        int totalOutcomes = 6;
+
+        int commonDivisor = gcd(winningOutcomes, totalOutcomes);
+
+        System.out.println(winningOutcomes/commonDivisor + "/" + totalOutcomes/commonDivisor);
+    }
+}
