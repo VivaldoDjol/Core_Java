5644d9e5ca43edd4b0593e697c1042010d11f77b
Gozzerks <VivaldoDjol@users.noreply.github.com>
2025-08-19 12:51:13 +0100

    Solve aMaximumGCD.java

---
+++ CodeForces/aMaximumGCD.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int tests = sc.nextInt();
+
+        while (tests-- > 0) {
+            int n = sc.nextInt();
+            System.out.println(n/2);
+        }
+    }
+}
