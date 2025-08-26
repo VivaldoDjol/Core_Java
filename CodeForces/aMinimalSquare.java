d79e5e16d7a8e32e865499b7cc3102de4cd9b68f
Gozzerks <VivaldoDjol@users.noreply.github.com>
2025-08-26 13:25:32 +0100

    Solve aMinimalSquare.java

---
+++ CodeForces/aMinimalSquare.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int tests = sc.nextInt();
+
+        while(tests-- > 0){
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+
+            int side = Math.min(Math.max(a * 2, b), Math.max(a, b * 2));
+            System.out.println(side * side);
+        }
+    }
+}
