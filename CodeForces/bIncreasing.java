972ac607e6581a8ab8e9b5ad863dd4e39c78c07c
Gozzerks <VivaldoDjol@users.noreply.github.com>
2025-08-26 13:25:00 +0100

    Solve bIncreasing.java

---
+++ CodeForces/bIncreasing.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int tests = sc.nextInt();
+
+        while (tests-- > 0) {
+            int n = sc.nextInt();
+            int[] arr = new int[n];
+
+            for (int i = 0; i < n; i++) {
+                arr[i] = sc.nextInt();
+            }
+
+            int count = 0;
+            for (int i = 0; i < n; i++) {
+                for (int j = i + 1; j < n; j++) {
+                    if (arr[i] == arr[j]) {
+                        count++;
+                    }
+                }
+            }
+            if (count == 0) {
+                System.out.println("YES");
+            } else {
+                System.out.println("NO");
+            }
+        }
+    }
+}
