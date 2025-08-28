74514d77cebd64a99f84c44b0d019bedecc91059
Gozzerks <VivaldoDjol@users.noreply.github.com>
2025-08-28 07:48:42 +0100

    Solve aHalloumiBoxes.java

---
+++ CodeForces/aHalloumiBoxes.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int tests = sc.nextInt();
+
+        while (tests-- > 0) {
+            int n = sc.nextInt();
+            int k = sc.nextInt();
+            int[] arr = new int[n];
+
+            for (int i = 0; i < n; i++) {
+                arr[i] = sc.nextInt();
+            }
+
+            if (isSorted(arr) || k > 1) {
+                System.out.println("YES");
+            } else {
+                System.out.println("NO");
+            }
+        }
+        sc.close();
+    }
+
+    private static boolean isSorted(int[] arr) {
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] < arr[i - 1]) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
