4a87ba222a6c7cd86d2b5807432b97727dcdfe19
Gozzerks <VivaldoDjol@users.noreply.github.com>
2025-10-10 13:26:07 +0100

    Solve aRaisingBacteria.java

---
+++ CodeForces/aRaisingBacteria.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        long x = sc.nextLong();
+        int count = 0;
+
+        while (x > 0) {
+            if ((x & 1) == 1) count++;
+            x >>= 1;
+        }
+        sc.close();
+        System.out.println(count);
+    }
+}
