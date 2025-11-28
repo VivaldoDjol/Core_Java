5cf49c8fc537b3b9c8fc20b4965f41c7070bb7da
Gozzerks <VivaldoDjol@users.noreply.github.com>
2025-11-28 23:36:43 +0000

    Solve aSystemOfEquations.java

---
+++ CodeForces/aSystemOfEquations.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        sc.close();
+
+        int count = 0;
+        for (int a = 0; a * a <= n; a++) {
+            int b = n - a * a;
+            if (b < 0) continue;
+            if (a + b * b == m) {
+                count++;
+            }
+        }
+        System.out.println(count);
+    }
+}
