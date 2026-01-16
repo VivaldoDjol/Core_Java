17c6e8951a1f9ed15b9ca7a354fc56e5e8603128
Gozzerks <VivaldoDjol@users.noreply.github.com>
2026-01-16 11:56:23 +0000

    Solve aAndThenThereWereK.java

---
+++ CodeForces/aAndThenThereWereK.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int tests = sc.nextInt();
+
+        while (tests-- > 0) {
+
+            int n = sc.nextInt();
+            n |= n >> 1;
+            n |= n >> 2;
+            n |= n >> 4;
+            n |= n >> 8;
+            n |= n >> 16;
+
+            n = n >> 1;
+            System.out.println(n);
+        }
+    }
+}
